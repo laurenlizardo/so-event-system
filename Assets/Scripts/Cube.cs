c2409c3775ca363cb132060dd4da5f39b5ec5eb0
laurenlizardo <me@lauren-lizardo.com>
2020-11-08 20:57:54 -0800

    Utilized MaterialPropertyBlock for color changing

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -1,15 +1,28 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 [RequireComponent( typeof( MeshRenderer ) )]
 public class Cube : MonoBehaviour
 {
-    public MeshRenderer MeshRenderer => GetComponent<MeshRenderer>();
+    private static readonly int _shaderProperty = Shader.PropertyToID("_Color");
+    private MaterialPropertyBlock _mpb;
+    public MaterialPropertyBlock Mpb
+    {
+        get
+        {
+            if( _mpb == null )
+            {
+                _mpb = new MaterialPropertyBlock();
+            }
+            return _mpb;
+        }
+    }
 
+    public MeshRenderer MeshRenderer => GetComponent<MeshRenderer>();
+    
     public void ChangeColor( Color col )
     {
-        MeshRenderer.material.color = col;
+        Mpb.SetColor( _shaderProperty, col );
+        MeshRenderer.SetPropertyBlock( Mpb );
     }
 
     public void ChangeToRandomColor()
